fb39f44ba4c4fafa42ac703c3957cfd3292dff57
Debora Cristina <fcristinadebora@gmail.com>
2019-05-27 18:54:37 -0300

    first commit, base and secure backend

---
+++ migrations/20190526152959_create_table_tasks.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('tasks', table => {
+      table.increments('id').primary()
+      table.string('desc').notNull()
+      table.datetime('doneAt')
+      table.integer('userId').references('id').inTable('users').notNull()
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('tasks')
+};
